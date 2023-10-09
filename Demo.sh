ba3bcba36188089922a5718e4d73beac12d6cf11
Bibhuti Bhushan <bibhuti1221bhushan@gmail.com>
2023-10-09 18:53:52 +0530

    Done

--- Demo.sh
+++ Demo.sh
@@ -10,3 +10,48 @@ fi
 # The directory exists, so exit successfully
 lsblk
 ls -h
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nModifying Pacman Configuration...\n"
+
+# enable options "color", "ParallelDownloads", "multilib (32-bit) repository"
+sed -i 's #Color Color ; s #ParallelDownloads ParallelDownloads ; s #\[multilib\] \[multilib\] ; /\[multilib\]/{n;s #Include Include }' /etc/pacman.conf
+
+echo -e "\nDone.\n\n"
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nPerforming Initialization of Pacman Keyring...\n"
+
+pacman-key --init
+
+pacman-key --populate archlinux
+
+echo -e "\nDone.\n\n"
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nAdding Fastest Mirror in Pacman Mirrorlist...\n"
+
+# save preferred configuration for the reflector systemd service
+echo -e "--save /etc/pacman.d/mirrorlist\n--country Sweden,Denmark\n--protocol https\n--score 10\n" > /etc/xdg/reflector/reflector.conf
+
+reflector --save /etc/pacman.d/mirrorlist --country Sweden,Denmark --protocol https --score 10 --verbose
+
+echo -e "\nDone.\n\n"
+
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nPerforming Pacstrap Operation...\n"
+
+# edit and adjust the "pkgs" file for desired packages (don't worry about any extra white spaces or new lines or comments as they will be omitted using sed and tr)
+pacstrap /mnt $(cat pkgs | sed 's #.*$  g' | tr '\n' ' ')
+
+echo -e "\nDone.\n\n"
+
+
