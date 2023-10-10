9c3c08c49f294c42016e8f8764d3993d9e7798f5
Bibhuti Bhushan <bibhuti1221bhushan@gmail.com>
2023-10-11 00:04:59 +0530

    Done

--- Demo.sh
+++ Demo.sh
@@ -1,57 +1,80 @@
 #!/bin/bash
 
-# Check if the directory exists
-if [ ! -d /sys/firmware/efi/efivars ]; then
-  # Give an error
-  echo "The directory /sys/firmwaress/efi/efivars does not exist."
+
+# COLOR VARIABLES :
+# ~~~~~~~~~~~~~~~~~ 
+BYELLOW='\e[93m'                         # BOLD YELLOW    
+BGREEN='\e[92m'                          # BOLD GREEN    
+BBLUE='\e[34m'                           # BOLD BLUE      
+BRED='\e[91m'                            # BOLD RED   
+RESET='\e[0m'                            # RESET       
+BOLD='\e[1m'                             # BOLD
+       
+# PRETTY PRINT FUNCTIONS :
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+Info_Print () {
+    echo -e "${BOLD}${BGREEN}[ ${BYELLOW}•${BGREEN} ] $1${RESET}"
+}
+
+Other_Print () {
+    echo -e "${BOLD}${BYELLOW}[ ${BGREEN}•${BYELLOW} ] $1${RESET}"
+}
+
+Error_Print () {
+    echo -e "${BOLD}${BRED}[ ${BBLUE}•${BRED} ] $1${RESET}"
+}
+
+# -------------------------- #
+# --- SET SOME VARIABLES --- #
+# -------------------------- #
+
+# SET NORMAL VARIABLES :
+# ~~~~~~~~~~~~~~~~~~~~~~
+USERNAME="Bibhuti"				         # SET USER NAME
+FULLNAME="Bibhuti Bhushan"		         # SET FULL NAME
+USERPASSWORD="\\"			      	     # SET USER PASSWORD
+ROOTPASSWORD="\\"			      	     # SET ROOT PASSWORD
+HOSTNAME="iTunes"			      	     # SET THE HOST NAME
+TIMEZONE="Asia/Kolkata"	    	         # SET TIME-ZONE
+LOCALE="en_US.UTF-8"			         # SET LOCALE
+LANGUAGE="en_US.UTF-8"                   # SET LOCALE.CONF
+KEYBOARD="us" 				      	     # SET KEYBOARD 
+
+# SET DISK VARIABLES :
+# ~~~~~~~~~~~~~~~~~~~~
+DISK=/dev/sda                            # SET DISK FOR INSTALL
+FILESYSTEM=ext4                          # SET FILESYSTEM
+
+# SET SIZE OF DRIVE :
+# ~~~~~~~~~~~~~~~~~~~
+BOOT_SIZE=550MiB                         # SET BOOT PARTITION SIZE
+SWAP_SIZE=8GiB                           # SET SWAP PARTITION SIZE
+ROOT_SIZE=35GiB                          # SET ROOT PARTITION SIZE
+HOME_SIZE=                               # REMAINING SPACE FOR HOME PARTITION
+
+# SET PACKAGES :
+# ~~~~~~~~~~~~~~
+KERNEL="linux-lts linux-lts-headers"     # SET KERNEL
+MICROCODE="intel-ucode"                  # SET MICROCODE
+EXTRA="git neovim"                       # EXTRA PACKAGES LIKE EDITOR...
+
+
+# ------------------------------- #
+# --- SCRIPT START FROM HERE ---  #
+# ------------------------------- #
+
+# CLEAR TERMINAL :
+# ~~~~~~~~~~~~~~~~
+clear
+
+# VERIFY BOOT MODE :
+# ~~~~~~~~~~~~~~~~~~
+if [ ! -d /sys/firmwares/efi/efivars ]; then
+  echo
+  Error_Print "YOU ARE NOT BOOTED IN UEFI"
+  echo
+  Error_Print "EXITING..."
+  echo
+  sleep 3
   exit 1
 fi
-
-# The directory exists, so exit successfully
-lsblk
-ls -h
-
-
-
-echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\nModifying Pacman Configuration...\n"
-
-# enable options "color", "ParallelDownloads", "multilib (32-bit) repository"
-sed -i 's #Color Color ; s #ParallelDownloads ParallelDownloads ; s #\[multilib\] \[multilib\] ; /\[multilib\]/{n;s #Include Include }' /etc/pacman.conf
-
-echo -e "\nDone.\n\n"
-
-
-
-echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\nPerforming Initialization of Pacman Keyring...\n"
-
-pacman-key --init
-
-pacman-key --populate archlinux
-
-echo -e "\nDone.\n\n"
-
-
-echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\nAdding Fastest Mirror in Pacman Mirrorlist...\n"
-
-# save preferred configuration for the reflector systemd service
-echo -e "--save /etc/pacman.d/mirrorlist\n--country Sweden,Denmark\n--protocol https\n--score 10\n" > /etc/xdg/reflector/reflector.conf
-
-reflector --save /etc/pacman.d/mirrorlist --country Sweden,Denmark --protocol https --score 10 --verbose
-
-echo -e "\nDone.\n\n"
-
-
-
-
-echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\nPerforming Pacstrap Operation...\n"
-
-# edit and adjust the "pkgs" file for desired packages (don't worry about any extra white spaces or new lines or comments as they will be omitted using sed and tr)
-pacstrap /mnt $(cat pkgs | sed 's #.*$  g' | tr '\n' ' ')
-
-echo -e "\nDone.\n\n"
-
-
--- Untitled-1.sh
+++ Untitled-1.sh
@@ -214,3 +214,73 @@ arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Your system is installed.  Type shutdown -h now to shutdown system and remove bootable media, then restart"
 read empty
+
+
+
+
+
+# Check if the directory exists
+if [ ! -d /sys/firmware/efi/efivars ]; then
+  # Give an error
+  echo "The directory /sys/firmwaress/efi/efivars does not exist."
+  exit 1
+fi
+
+# The directory exists, so exit successfully
+lsblk
+ls -h
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nModifying Pacman Configuration...\n"
+
+# enable options "color", "ParallelDownloads", "multilib (32-bit) repository"
+sed -i 's #Color Color ; s #ParallelDownloads ParallelDownloads ; s #\[multilib\] \[multilib\] ; /\[multilib\]/{n;s #Include Include }' /etc/pacman.conf
+
+echo -e "\nDone.\n\n"
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nPerforming Initialization of Pacman Keyring...\n"
+
+pacman-key --init
+
+pacman-key --populate archlinux
+
+echo -e "\nDone.\n\n"
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nAdding Fastest Mirror in Pacman Mirrorlist...\n"
+
+# save preferred configuration for the reflector systemd service
+echo -e "--save /etc/pacman.d/mirrorlist\n--country Sweden,Denmark\n--protocol https\n--score 10\n" > /etc/xdg/reflector/reflector.conf
+
+reflector --save /etc/pacman.d/mirrorlist --country Sweden,Denmark --protocol https --score 10 --verbose
+
+echo -e "\nDone.\n\n"
+
+
+
+
+echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nPerforming Pacstrap Operation...\n"
+
+# edit and adjust the "pkgs" file for desired packages (don't worry about any extra white spaces or new lines or comments as they will be omitted using sed and tr)
+pacstrap /mnt $(cat pkgs | sed 's #.*$  g' | tr '\n' ' ')
+
+echo -e "\nDone.\n\n"
+
+
+echo "--------------------------------------"
+echo "-- Bootloader Systemd Installation  --"
+echo "--------------------------------------"
+bootctl install --esp-path /mnt/boot
+cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root="$IN_DEVICE"1 rw
+EOF
