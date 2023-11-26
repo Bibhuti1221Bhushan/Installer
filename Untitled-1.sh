bebdd53d1e511b54c11091dffb7273f3534cdfa2
Bibhuti Bhushan <Bibhuti1221Bhushan@gmail.com>
2023-11-26 20:45:59 +0530

    Done

---
+++ Untitled-1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+T_CO_RED='\e[1;31m'
+T_CO_YELLOW='\e[1;33m'
+T_CO_GREEN='\e[1;32m'
+T_CO_BLUE='\e[1;34m'
+T_CO_GRAY='\e[1;30m'
+T_CO_NC='\e[0m'
+
+CURRENT_PROGRESS=0
+
+function script_print()
+{
+  echo -ne "$T_CO_BLUE[SCRIPT]$1$T_CO_NC"
+}
+
+function script_print_notify()
+{
+  echo -ne "$T_CO_BLUE[SCRIPT]$T_CO_NC$T_CO_GREEN-Notify- $1$T_CO_NC"
+}
+
+function script_print_error()
+{
+  echo -ne "$T_CO_BLUE[SCRIPT]$T_CO_NC$T_CO_RED-Error- $1$T_CO_NC"
+}
+
+function error_exit() {
+  script_print_error "$1\n\n"
+  exit 1
+}
+
+script_print "Hello World!\n"
+script_print_notify "Hello World!\n"
+script_print_error "Hello World!\n"
+error_exit "Hello World!\n"
